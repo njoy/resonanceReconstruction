7e89959e69c334b42667c547af383367f30f0705
Wim Haeck <whaeck@gmail.com>
2018-08-08 12:42:07 -0600

    Corrected a clang issue

--- src/resonanceReconstruction.hpp
+++ src/resonanceReconstruction.hpp
@@ -64,7 +64,7 @@ using CoulombSquaredSecondPerMeter = decltype( Coulomb() * Coulomb() *
 // hbar constant in eV s - taken from 2014 CODATA 
 constexpr Quantity< ElectronVoltSecond > hbar = 6.582119514e-16 * electronVolt * second;
 
-constexpr Quantity< RootElectronVolt > rootElectronVolt = sqrt( 1.0 * electronVolt );
+constexpr Quantity< RootElectronVolt > rootElectronVolt = 1.0 * unit::sqrt( electronVolt );
 constexpr Quantity< RootElectronVolt > rootElectronVolts = rootElectronVolt;
 
 // physical quantities
