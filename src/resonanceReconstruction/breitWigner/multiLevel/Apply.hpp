45bdc3d558dbf8825cd6d496c92a18f5da30324a
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-05-07 12:40:12 -0600

    Perhaps a better way of removing compiler warnings.

--- src/resonanceReconstruction/breitWigner/multiLevel/Apply.hpp
+++ src/resonanceReconstruction/breitWigner/multiLevel/Apply.hpp
@@ -3,5 +3,8 @@ protected:
   #include "resonanceReconstruction/breitWigner/multiLevel/Apply/src/build.hpp"
 
 public:
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreturn-type"
   #include "resonanceReconstruction/breitWigner/multiLevel/Apply/src/call.hpp"
+#pragma GCC diagnostic pop
 };
