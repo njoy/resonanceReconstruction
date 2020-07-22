472e6c13fac4d6a8a1796201a776a90caac53068
Wim Haeck <whaeck@gmail.com>
2020-07-22 14:49:24 -0600

    Cleaned up CMakeLists.txt for the rmatrix namespace

--- src/resonanceReconstruction/rmatrix/SpinGroup/src/calculateSqrtPDiagonal.hpp
+++
@@ -1,8 +0,0 @@
-template < typename Range >
-auto sqrtPenetrabilities( const Range& penetrabilities ) const {
-
-  return penetrabilities
-             | ranges::view::transform(
-                   [] ( const double penetrability ) -> double
-                      { return std::sqrt( penetrability ); } );
-}
