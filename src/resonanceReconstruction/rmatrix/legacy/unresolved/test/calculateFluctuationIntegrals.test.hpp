c141cce29e9c6b6723cc1029d4cce151fbb19de0
Wim Haeck <whaeck@gmail.com>
2020-07-22 20:56:56 -0600

    Updated fluctuation integral test results using NJOY2016 values, rearranged the arrays since they were not 10 arrays of 4 but 4 arrays of 10

--- src/resonanceReconstruction/rmatrix/legacy/unresolved/test/calculateFluctuationIntegrals.test.hpp
+++ src/resonanceReconstruction/rmatrix/legacy/unresolved/test/calculateFluctuationIntegrals.test.hpp
@@ -5,61 +5,70 @@ SCENARIO( "calculateFluctuationIntegrals" ) {
     THEN( "the appropriate fluctuation integrals are calculated "
           "when all widths are non-zero" ) {
 
-      Degrees degrees( 1, 1, 1, 1 );
-      Widths widths( 1.0 * electronVolts, 2.0 * electronVolts,
-                     3.0 * electronVolts, 4.0 * electronVolts );
+      Degrees degrees( 1, 0, 2, 3 );
+      Widths widths( 4.75405e-2 * electronVolts, 4.07e-2 * electronVolts,
+                     2.842 * electronVolts, 0.402 * electronVolts );
       FluctuationIntegrals integrals =
       calculateFluctuationIntegrals( widths, degrees );
 
-      CHECK( 1. / electronVolts == integrals.elastic );
-      CHECK( 2. / electronVolts == integrals.capture );
-      CHECK( 3. / electronVolts == integrals.fission );
-      CHECK( 4. / electronVolts == integrals.competition );
+      CHECK( 1.5667666623387133 == Approx( integrals.elastic.value ) );
+      CHECK( 0.56993816408468989 == Approx( integrals.capture.value ) );
+      CHECK( 0.25126381048572832 == Approx( integrals.fission.value ) );
+      CHECK( 0.4680023731 == Approx( integrals.competition.value ) );
+
+      // REMARK: NJOY2016 does not calculate the fluctuation integral for
+      //         the competitive reaction
     } // THEN
 
     THEN( "the appropriate fluctuation integrals are calculated "
           "when all widths are non-zero except for fission" ) {
 
-      Degrees degrees( 1, 1, 0, 1 );
-      Widths widths( 1.0 * electronVolts, 2.0 * electronVolts,
-                     0.0 * electronVolts, 4.0 * electronVolts );
+      Degrees degrees( 1, 0, 0, 3 );
+      Widths widths( 4.75405e-2 * electronVolts, 4.07e-2 * electronVolts,
+                     0.0 * electronVolts, 2.842 * electronVolts );
       FluctuationIntegrals integrals =
       calculateFluctuationIntegrals( widths, degrees );
 
-      CHECK( 1. / electronVolts == integrals.elastic );
-      CHECK( 2. / electronVolts == integrals.capture );
-      CHECK( 0. / electronVolts == integrals.fission );
-      CHECK( 4. / electronVolts == integrals.competition );
+      CHECK( 1.7591280230875452 == Approx( integrals.elastic.value ) );
+      CHECK( 0.65378100220430613 == Approx( integrals.capture.value ) );
+      CHECK( 0. == Approx( integrals.fission.value ) );
+      CHECK( 0.3130757611 == Approx( integrals.competition.value ) );
+
+      // REMARK: NJOY2016 does not calculate the fluctuation integral for
+      //         the competitive reaction
     } // THEN
 
     THEN( "the appropriate fluctuation integrals are calculated "
           "when all widths are non-zero except for competition" ) {
 
-      Degrees degrees( 1, 1, 1, 0 );
-      Widths widths( 1.0 * electronVolts, 2.0 * electronVolts,
-                     3.0 * electronVolts, 0.0 * electronVolts );
+      // values tested against NJOY2016 using ENDF/B-VIII.0 Pu239
+      // at 2500 eV
+
+      Degrees degrees( 1, 0, 2, 0 );
+      Widths widths( 4.75405e-2 * electronVolts, 4.07e-2 * electronVolts,
+                     2.842 * electronVolts, 0.0 * electronVolts );
       FluctuationIntegrals integrals =
       calculateFluctuationIntegrals( widths, degrees );
 
-      CHECK( 1. / electronVolts == integrals.elastic );
-      CHECK( 2. / electronVolts == integrals.capture );
-      CHECK( 3. / electronVolts == integrals.fission );
-      CHECK( 0. / electronVolts == integrals.competition );
+      CHECK( 2.2806311 == Approx( integrals.elastic.value ) );
+      CHECK( 0.8945772 == Approx( integrals.capture.value ) );
+      CHECK( 0.30087207 == Approx( integrals.fission.value ) );
+      CHECK( 0. == Approx( integrals.competition.value ) );
     } // THEN
 
     THEN( "the appropriate fluctuation integrals are calculated "
           "when only elastic and capture widths are non-zero" ) {
 
-      Degrees degrees( 1, 1, 0, 0 );
-      Widths widths( 1.0 * electronVolts, 2.0 * electronVolts,
+      Degrees degrees( 1, 0, 0, 0 );
+      Widths widths( 4.75405e-2 * electronVolts, 4.07e-2 * electronVolts,
                      0.0 * electronVolts, 0.0 * electronVolts );
       FluctuationIntegrals integrals =
       calculateFluctuationIntegrals( widths, degrees );
 
-      CHECK( 1. / electronVolts == integrals.elastic );
-      CHECK( 2. / electronVolts == integrals.capture );
-      CHECK( 0. / electronVolts == integrals.fission );
-      CHECK( 0. / electronVolts == integrals.competition );
+      CHECK( 14.395389203673844 == Approx( integrals.elastic.value ) );
+      CHECK( 7.7551848262740855 == Approx( integrals.capture.value ) );
+      CHECK( 0. == Approx( integrals.fission.value ) );
+      CHECK( 0. == Approx( integrals.competition.value ) );
     } // THEN
   } // GIVEN
 } // SCENARIO
