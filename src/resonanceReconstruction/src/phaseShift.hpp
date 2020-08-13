e991c124a8203f55462c8b472a1b3890ea69f6a9
Martin Staley <kipnflip@gmail.com>
2020-08-13 16:01:43 -0600

    Replaced lambda with a straight switch.

--- src/resonanceReconstruction/src/phaseShift.hpp
+++ src/resonanceReconstruction/src/phaseShift.hpp
@@ -3,15 +3,14 @@ inline double phaseShift( const int L, const double channelRatio ){
     return channelRatio;
 
   const double rho2 = channelRatio * channelRatio;
-  const double offset = [&]{
-    switch( L ){
-      case 1: return 1.0;
-      case 2: return 3/(3-rho2);
-      case 3: return (15-rho2)/(15-6*rho2);
-      case 4: return (105-10*rho2)/(105+rho2*(rho2-45));
-      default: throw std::exception();
-    }
-  }();
+  double offset;
+  switch( L ){
+    case 1: offset = 1; break;
+    case 2: offset = 3/(3-rho2); break;
+    case 3: offset = (15-rho2)/(15-6*rho2); break;
+    case 4: offset = (105-10*rho2)/(105+rho2*(rho2-45)); break;
+    default: throw std::exception();
+  };
   
   return channelRatio - std::atan( channelRatio * offset );
 }
