8c326f37974d2a9638f48de51db43317d3ffa7eb
Martin Staley <kipnflip@gmail.com>
2020-08-13 13:10:49 -0600

    Miscellaneous simplifications. The most significant change is with pack.hpp. Then, minor but worthwhile simplifications and consistent-izations elsewhere.

--- src/resonanceReconstruction/src/channelRadius.hpp
+++ src/resonanceReconstruction/src/channelRadius.hpp
@@ -1,10 +1,6 @@
-inline auto channelRadius( double atomicWeightRatio ){
+inline auto channelRadius( const double atomicWeightRatio ){
   constexpr double amu = 1.00866491582;
   const auto scalar =
     0.123 * std::pow( amu * atomicWeightRatio, 1. / 3. ) + 0.08;
-  
-  return
-    [ radius = scalar * rootBarn ]
-    ( Quantity< ElectronVolts > /* energy */ )
-    { return radius; };
+  return radius(scalar);
 }
--- src/resonanceReconstruction/src/phaseShift.hpp
+++ src/resonanceReconstruction/src/phaseShift.hpp
@@ -1,17 +1,17 @@
-inline double phaseShift( const int l, const double channelRatio ){
-  if ( not l ){ return channelRatio; }
+inline double phaseShift( const int L, const double channelRatio ){
+  if ( L == 0 )
+    return channelRatio;
 
-  const auto rho2 = channelRatio * channelRatio;
-  const auto offset = [&]{
-    switch( l ){
-    case 1: return channelRatio;
-    case 2: return channelRatio * 3. / ( 3. - rho2 );
-    case 3: return channelRatio * ( 15. - rho2 ) / ( 15. - 6. * rho2 );
-    case 4: return channelRatio * ( 105. - 10. * rho2 )
-                                  / ( 105. + rho2 * ( -45. + rho2 ) );
-    default: throw std::exception();
+  const double rho2 = channelRatio * channelRatio;
+  const double offset = [&]{
+    switch( L ){
+      case 1: return 1.0;
+      case 2: return 3/(3-rho2);
+      case 3: return (15-rho2)/(15-6*rho2);
+      case 4: return (105-10*rho2)/(105+rho2*(rho2-45));
+      default: throw std::exception();
     }
   }();
   
-  return channelRatio - std::atan( offset );
+  return channelRatio - std::atan( channelRatio * offset );
 }
