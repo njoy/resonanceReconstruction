68ffc6df7aec6edf50a4aa950b92a7fd397cfe5a
Wim Haeck <whaeck@gmail.com>
2020-08-24 08:41:23 -0600

    Set elementary dependence to v0.2.1

--- cmake/resonanceReconstruction_dependencies.cmake
+++ cmake/resonanceReconstruction_dependencies.cmake
@@ -32,7 +32,7 @@ FetchContent_Declare( dimwits
 
 FetchContent_Declare( elementary
     GIT_REPOSITORY  http://github.com/njoy/elementary
-    GIT_TAG         feature/resolve
+    GIT_TAG         v0.2.1
     )
 
 
