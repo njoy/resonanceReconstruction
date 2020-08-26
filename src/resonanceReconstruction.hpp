e43e1c2c8f611eff92e121fe5f9599f1bd9e4583
Wim Haeck <whaeck@gmail.com>
2020-08-26 12:20:08 -0600

    Removed comments to re-enable the older RM implementation for now

--- src/resonanceReconstruction.hpp
+++ src/resonanceReconstruction.hpp
@@ -53,7 +53,7 @@ namespace ENDF = ENDFtk::resonanceParameters;
 #include "resonanceReconstruction/ZeroWidth.hpp"
 
 #include "resonanceReconstruction/breitWigner.hpp"
-//#include "resonanceReconstruction/reichMoore.hpp"
+#include "resonanceReconstruction/reichMoore.hpp"
 
 // physical constants and quantities
 
