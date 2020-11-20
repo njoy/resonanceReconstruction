0f8f15093970ccbc8aea4ed10fcff23da39a24f6
Wim Haeck <whaeck@gmail.com>
2020-11-20 14:12:44 -0700

    Corrected a comment on the mandelstam parameter

--- src/resonanceReconstruction/rmatrix/Channel/src/mandelstam.hpp
+++ src/resonanceReconstruction/rmatrix/Channel/src/mandelstam.hpp
@@ -3,7 +3,7 @@
  *         particle pair at a given energy
  *
  *  The Mandelstam variable s is defined as:
- *     s = ( ( ma + mb ) c^2 )^2 + mb c^2 energy
+ *     s = ( ( ma + mb ) c^2 )^2 + 2 mb c^2 energy
  *  in which ma and mb are the atomic masses of the first and second particles
  *  in the particle pair and c is the light speed (converting the mass into
  *  energy).
