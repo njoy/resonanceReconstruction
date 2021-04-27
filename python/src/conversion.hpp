ecac3fcc553fbb020bb2c6af139a4657303d6819
Wim Haeck <whaeck@gmail.com>
2021-04-27 10:44:05 -0600

    Saving work

--- python/src/conversion.hpp
+++ python/src/conversion.hpp
@@ -12,10 +12,17 @@ using AtomicMass = njoy::resonanceReconstruction::AtomicMass;
 using ElectricalCharge = njoy::resonanceReconstruction::ElectricalCharge;
 using ChannelRadius = njoy::resonanceReconstruction::ChannelRadius;
 using Energy = njoy::resonanceReconstruction::Energy;
+using ReducedWidth = njoy::resonanceReconstruction::ReducedWidth;
 using QValue = njoy::resonanceReconstruction::QValue;
 
 template < typename Unit, typename Magnitude >
 Magnitude removeUnit( const Quantity< Unit, Magnitude >& quantity ) {
+
+  return quantity.value;
+}
+
+template < typename Unit, typename Magnitude >
+std::vector< double > removeArrayUnit( const std::vector< Quantity< Unit, Magnitude > >& array ) {
 
   return quantity.value;
 }
@@ -40,9 +47,36 @@ inline Energy toEnergy( double value ) {
   return value * dimwits::electronVolt;
 }
 
+inline ReducedWidth toReducedWidth( double value ) {
+
+  return value * dimwits::rootElectronVolt;
+}
+
 inline QValue toQValue( double value ) {
 
   return value * dimwits::electronVolt;
+}
+
+inline std::vector< Energy > toEnergyArray( const std::vector< double > array ) {
+
+  std::vector< Energy > energies;
+  energies.reserve( array.size() );
+  for ( auto value : array ) {
+
+    energies.push_back( toEnergy( value ) );
+  }
+  return energies;
+}
+
+inline std::vector< ReducedWidth > toReducedWidthArray( const std::vector< double > array ) {
+
+  std::vector< ReducedWidth > widths;
+  widths.reserve( array.size() );
+  for ( auto value : array ) {
+
+    widths.push_back( toReducedWidth( value ) );
+  }
+  return widths;
 }
 
 #endif
